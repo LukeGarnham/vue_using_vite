740371417245f692014cc7b3ad22f3e4c2c2796c
Luke Garnham <lukegarnham@msn.com>
2026-09-22 08:23:07 +0100

    Added a Team page and constructed components using Pinia to store team data. Installed Tailwind.css - Team page is not properly styled.

---
+++ src/components/Teams/TeamHeader.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { useTeamStore } from '@/stores/TeamStore'
+
+let team = useTeamStore()
+</script>
+
+<template>
+  <header class="justify-between">
+    <div>
+      <button
+        class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded disabled:bg-gray-400"
+        :disabled="!team.spotsRemaining"
+      >
+        Add Member ({{ team.spotsRemaining }} Spots Left)
+      </button>
+    </div>
+    <div>
+      <h3 class="inline-flex items-center text-3xl relative">
+        {{ team.name }} Team
+        <div
+          class="bg-green-400 w-5 h-5 text-xs text-white rounded-full flex justify-center items-center absolute -right-4 -top-2"
+        >
+          {{ team.spots }}
+        </div>
+      </h3>
+    </div>
+  </header>
+</template>
---
+++ src/views/TeamView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import TeamHeader from '@/components/Teams/TeamHeader.vue'
+import TeamMembers from '@/components/Teams/TeamMembers.vue'
+import TeamFooter from './TeamFooter.vue'
+import { useTeamStore } from '@/stores/TeamStore.js'
+
+let team = useTeamStore()
+team.fill()
+</script>
+<template>
+  <TeamHeader></TeamHeader>
+  <div class="place-self-center flex flex-col gap-y-3">
+    <TeamMembers></TeamMembers>
+  </div>
+  <TeamFooter></TeamFooter>
+</template>
