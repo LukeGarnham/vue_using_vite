498b5f68c67726b01e19434ddfbb8bf23a990704
Luke Garnham <lukegarnham@msn.com>
2026-09-22 22:08:57 +0100

    Utilised <Teleport> to move the modal to the bottom of the body.

--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -2,31 +2,19 @@
 import TeamHeader from '@/components/Teams/TeamHeader.vue'
 import TeamMembers from '@/components/Teams/TeamMembers.vue'
 import TeamFooter from './TeamFooter.vue'
-import Modal from '@/components/Modal.vue'
 import { useTeamStore } from '@/stores/TeamStore.js'
-import { ref } from 'vue'
+import AddMemberModal from '@/components/Teams/AddMemberModal.vue'
 
 let team = useTeamStore()
 
-let showModal = ref(false)
 team.fill()
 </script>
 <template>
-  <TeamHeader @add="showModal = true"></TeamHeader>
+  <TeamHeader></TeamHeader>
   <div class="place-self-center flex flex-col gap-y-3">
     <TeamMembers></TeamMembers>
   </div>
   <TeamFooter></TeamFooter>
 
-  <Modal :show="showModal" @close="showModal = false">
-    <template #default>
-      <p>Need to add a new member to your team?</p>
-      <form class="mt-6">
-        <div class="flex gap-1">
-          <input type="email" placeholder="Email address..." class="flex-1" />
-          <button>Add</button>
-        </div>
-      </form>
-    </template>
-  </Modal>
+  <AddMemberModal></AddMemberModal>
 </template>
